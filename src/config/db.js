f81e79371ae81c57cd34acf7c4390e271ef3b529
Amr <amrkhaled11203@gmail.com>
2026-02-26 22:05:10 +0200

    deploy fix

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error(
+      "MONGO_URI environment variable is not defined. " +
+        "Please set it in your Railway Variables (or .env file locally).",
+    );
+  }
   await mongoose.connect(process.env.MONGO_URI);
   console.log("MongoDB Connected");
 };
