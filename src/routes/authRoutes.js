c6b880ca63b51db4fc926816b5da37492bcc5072
Amr <amrkhaled11203@gmail.com>
2026-02-27 14:13:31 +0200

    protect register

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { login, register } from "../controllers/authController.js";
+import protect from "../middleware/authHandler.js";
 
 const router = express.Router();
 
 router.post("/login", login);
-router.post("/register", register);
+
+// Protected: only an existing admin can register new users
+router.post("/register", protect, register);
 
 export default router;
