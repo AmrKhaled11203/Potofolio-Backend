b81b57a217a1c88d3c62079c9d02e50019eddaf0
Amr <amrkhaled11203@gmail.com>
2026-02-28 01:37:31 +0200

    fix uploading

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ import notFound from "./middleware/notFoundHandler.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
