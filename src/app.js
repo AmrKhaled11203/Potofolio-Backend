8160725e8ba31aac25eac138cdb77c7faacaca8c
Amr <amrkhaled11203@gmail.com>
2026-03-01 16:52:10 +0200

    fix login

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
-app.options("*", cors());
+app.options("(.*)", cors());
 app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(morgan("dev"));
 app.use("/uploads", express.static(uploadDir));
